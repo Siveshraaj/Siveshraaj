406c182e212facd6ba7b6f4e3c4f41dfcfd56c0a
Siveshraaj <92732548+Siveshraaj@users.noreply.github.com>
2021-10-18 22:07:11 +0530

    Create Positive .C
    The file prints Positive on the screen

---
+++ Positive .C
@@ -0,0 +1,30 @@
+1) 
+
+# Python program to print positive Numbers in given range
+ 
+start, end = -14, -7, 5,12,64
+
+for num in range(start, end + 1):
+     
+    if num >= 0:
+        print(num, end = " ")
+\
+Output:
+       12,5,64
+
+
+2) 
+# Python program to print positive Numbers in given range
+ 
+start = int(input("Enter the start of range: "))
+end = int(input("Enter the end of range: "))
+
+for num in range(start, end + 1):
+     
+    if num >= 0:
+        print(num, end = " ")
+Output:
+
+Enter the start of range: -95
+Enter the end of range: 14
+[12, 14,3]
